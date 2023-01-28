bfa0bca08aa8572b4e67bd035a9aeb6575eb279f
ZZardash <sertac51yurtaslan34@hotmail.com>
2023-01-28 09:38:17 +0300

    New functions added.

--- script.js
+++ script.js
@@ -258,7 +258,150 @@ number = 5;
 console.log(Number.isInteger(number))
 console.log(Number.isSafeInteger(number))
 
+//------------------ARRAYS-------------------
 
+const fruits = ["Melon","Apple","Cherry"];
+fruits[0] = "Watermelon";
 
+function printArray(x){
+    for (let index = 0; index < x.length; index++)
+    {
+        const element = x[index];
+        console.log(element);
+    }
+}
+
+//Creating HTML list with array
+let fLen = fruits.length;
+
+let _txt = "<ul>";
+for (let i = 0; i < fLen; i++) {
+  _txt += "<li>" + fruits[i] + "</li>";
+}
+_txt += "</ul>"; 
+/*txt = "
+<ul>
+    <li>Watermelon</li>
+    <li>Apple</li>
+    <li>Cherry</li>
+</ul>";
+*/
+
+//Is Array ? 
+function isArray(x){
+    var isArray = Array.isArray(x);
+    return isArray;
+}
+console.log(isArray(fruits));
+
+//Sort
+fruits.sort();
+printArray(fruits);
+
+//Reverse.
+fruits.reverse();
+printArray(fruits);
+
+//---------------NUMBER METHODS---------------
+
+//toString
+var a = 312312;
+a = a.toString();
+console.log(typeof(a)+": "+ a);
+
+//toExponential
+var b = 3.145;
+b = b.toExponential(2);
+console.log("Exponential notation: "+b);
+
+//toFixed
+var c = 324123;
+c = c.toFixed(2); //Zero count after comma [must be(0,100)]
+console.log("Fixed: "+c);
+
+//toPrecision
+var d = 39999;
+d = d.toPrecision(2);
+console.log(d);
+
+//valueOf
+var e = 12312;
+e = e.valueOf();
+console.log(e);
+
+//random
+var _a = Math.random(); //Always return number lower than 1
+console.log("Random number: "+_a);
+
+// Returns a random integer from 0 to 9:
+console.log(Math.floor(Math.random() * 10));
+
+//Proper random function
+function rnd(min, max){
+    return Math.floor(Math.random() * (max-min)) + min;
+}
+/*Example:
+    a = rnd(5, 10);
+    a => 0,1 + 5 or 3+5 or 0,1231+5
+*/
+
+randomInt = rnd(3,77);
+console.log("Random integer between 3,77: "+randomInt);
+
+console.log("12"<"2"); //Alphabtic comparison
+
+//-----------------SETS-----------------
+
+const letters = new Set(["a","b","c","d"]);
+letters.add("e");
+letters.add("f");
+
+var txt123 = "";
+letters.forEach (function(value) {
+  txt123 += value;
+});
+
+console.log(letters.values());
+
+//-----------------MAPS-----------------
+
+//Create a map
+const cities = new Map([
+    ["New York", 32],
+    ["Los Angeles", 17],
+    ["Boston", 22]
+]);
+
+//Add or change existing
+cities.set("New York", 36);
+cities.set("Istanbul", 34);
+
+//Get
+cities.get("New York"); //Returns 36
+
+//Size
+console.log(cities.size);
+
+//Delete
+cities.delete("New York");
+console.log(cities.size);
+
+//Has
+console.log(cities.has("New York"));
+
+//List all entries
+let allEntries = "";
+for (const x of cities.entries()){
+    allEntries += x+"\n";
+}
+
+console.log(allEntries);
+
+//-----------------TYPE CASTING-----------------
+
+let string = "5";
+let numberString = +string; //Cast with unary (+) operator
+
+console.log(typeof(numberString));
 
 
